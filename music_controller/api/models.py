52e9e3b5c6263555cc6b2d6aea656c284067df4a
hector <hectorrrivera1010@gmail.com>
2023-11-14 13:04:51 -0600

    [add] serializer and view

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -19,7 +19,7 @@ traduce a SQL cuando lo ejecutamos, asi solo tenemos que usar codigo de python y
 nada en SQL
 """
 class Room(models.Model):
-    code = models.CharField(max_length=10, default="", unique=True)
+    code = models.CharField(max_length=10, default=code_generator, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False,default=False)
     votes_to_skip = models.IntegerField(null=False,default=1)
